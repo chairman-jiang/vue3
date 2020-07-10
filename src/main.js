88b062de770739d9c64bccc3c6c6a79d80a5fc26
徐晨淼 <17600000559@163.com>
2020-07-10 17:56:58 +0800

    testing

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import { reactive, effect } from './core'
 const obj = reactive({ name: '456', age: 10 });
 
 effect(() => {
-  console.log(obj.name, 'value')
-  console.log('触发effect');
+  console.log(obj.name, 'effect');
+})
+
+effect(() => {
+  console.log(obj.name, 'effect2');
 })
 
 setTimeout(() => {
   obj.name = '请问'
   console.log('timeout')
-}, 1000);
+}, 3000);
+
+// setTimeout(() => {
+//   console.log(obj.name, 'name');
+// }, 3000)
